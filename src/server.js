32db50166aed6f53ec7f1d5aedb63a21d44af828
hendry <hendrry16@gmail.com>
2025-08-18 17:06:56 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -6,9 +6,13 @@ const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 const ClientError = require('./exceptions/ClientError');
+const UsersService = require('./services/postgres/UsersService');
+const users = require('./api/users');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -20,20 +24,30 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   server.ext('onPreResponse', (request, h) => {
     const { response } = request;
 
     if (response instanceof ClientError) {
       const newResponse = h.response({
-        status: 'fail', message: response.message,
+        status: 'fail',
+        message: response.message,
       });
       newResponse.code(response.statusCode);
       return newResponse;
