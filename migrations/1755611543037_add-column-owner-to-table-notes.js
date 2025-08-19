b66fc0a6f4dae4520879b5176c760c2b6b3aad47
hendry <hendrry16@gmail.com>
2025-08-19 22:03:11 +0700

    authorization implemented

---
+++ migrations/1755611543037_add-column-owner-to-table-notes.js
@@ -0,0 +1,11 @@
+export const up = (pgm) => {
+  pgm.addColumn('notes', {
+    owner: {
+      type: 'VARCHAR(50)',
+    },
+  });
+};
+
+export const down = (pgm) => {
+  pgm.dropColumn('notes', 'owner');
+};
